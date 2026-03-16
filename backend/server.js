8921a19e7c8a52d23f3693d1f1fb031e691d68b0
eshwar <eshwarprudhvi2005@gmail.com>
2026-03-16 21:40:43 +0530

    fix cors origin

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
    origin: [
       "http://localhost:5173",
-      "https://blog-app-prudhvi.netlify.app/"
+      "https://blog-app-prudhvi.netlify.app"
      
     ],
     credentials: true,
